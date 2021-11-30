8a673e8c66620a471395f9a2421d3379a1ddaf98
Moks <89726455+moks24@users.noreply.github.com>
2021-11-30 16:21:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
-public class main {
-    public static void main(String[] args){
+public class Main {
+    public static void Main(String[] args) {
         int initialAccount = 200;
         int replenishmentAmount = 1200;
         int percent = 100;
-        int bonus = replenishmentAmount/percent;
-
-        if (replenishmentAmount > 1000){
-            bonus = replenishmentAmount/percent;
-        }else {
+        int bonus = replenishmentAmount / percent;
+        if (replenishmentAmount > 1000) {
+            bonus = replenishmentAmount / percent;
+        } else {
             bonus = 0;
         }
-        int check = initialAccount +replenishmentAmount + bonus;
+        int check = initialAccount + replenishmentAmount + bonus;
         System.out.println(bonus);
         System.out.println(check);
-        }
-
     }
 
+}
